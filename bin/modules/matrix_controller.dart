6ba4729897081e146fe1f8ad4a081a85f989cd12
f1rrock <flyrock000@gmail.com>
2022-09-26 15:11:59 +0300

    first working version with all comments

--- bin/modules/matrix_controller.dart
+++ bin/modules/matrix_controller.dart
@@ -5,15 +5,19 @@ import 'matrix.dart';
 class MatrixController {
   /// this field associate with [Matrix] class
   final Matrix matrix;
+
   /// error in answer field
   final double epsilon;
+
   /// amount of digits after the dot in [double] num
   /// this field set automatically once at initialize part at
   /// [this] constructor
   final int fraction;
+
   /// answer column of [double], here will be answers
   /// on the given equation system
   final List<double> xx;
+
   /// declare of file local method, which convert [num] to [String]
   /// with plus and minus sign before
   String _toStringWithSign(double num) {
@@ -21,6 +25,7 @@ class MatrixController {
         ? '+${num.toStringAsFixed(fraction)}'
         : num.toStringAsFixed(fraction);
   }
+
   /// print each equal as linear function with each diagonal element
   void printEqualAsLine() {
     for (var i = 0; i < matrix.size; i++) {
@@ -28,17 +33,20 @@ class MatrixController {
       var rightSideString =
           '${(1 / matrix.grid[i][i]).toStringAsFixed(fraction)}'
           '*(${matrix.grid[i][matrix.size].toStringAsFixed(fraction)}';
+
       /// add all the rest answers to right side of equation
       for (var j = 0; j < matrix.size; j++) {
         if (i != j) {
           rightSideString +=
-          '${_toStringWithSign(-matrix.grid[i][j])}*x${j + 1}';
+              '${_toStringWithSign(-matrix.grid[i][j])}*x${j + 1}';
         }
       }
+
       /// print all equation on each diagonal element
       print('x${i + 1} = $rightSideString)');
     }
   }
+
   /// [tryNormalize] function tries to normalize
   /// (absolute value of diagonal element is greater than sum
   /// of each absolute value element in row)
@@ -61,11 +69,13 @@ class MatrixController {
           index = i;
         }
       }
+
       /// [this] function will return false if normality
       /// row for this iteration not found
       if (index < 0) {
         return false;
       }
+
       /// switch rows if this need
       if (index != k) {
         var temp = matrix.grid[index];
@@ -73,10 +83,12 @@ class MatrixController {
         matrix.grid[k] = temp;
       }
     }
+
     /// return true if given matrix modified
     /// to matrix where normality < q < 1; q in (0; 1)
     return true;
   }
+
   /// [this] function converts given matrix
   /// to upper triangle view
   bool convertToTriangleView() {
@@ -85,9 +97,11 @@ class MatrixController {
       /// to avoid [DivideByZeroException], because all zero elements
       /// will be in the bottom of the matrix
       var max = matrix.grid[k][k].abs();
+
       /// declare index of row that had
       /// max absolute element in column
       var index = k;
+
       /// find row with max element in [k] column
       for (var i = k + 1; i < matrix.size; i++) {
         if (matrix.grid[i][k].abs() > max) {
@@ -95,6 +109,7 @@ class MatrixController {
           index = i;
         }
       }
+
       /// switch rows between elem and [max]
       if (max < epsilon) {
         /// [max] element is zero,
@@ -103,14 +118,17 @@ class MatrixController {
         print('\nНельзя получить точное решение '
             'данной системы т.к. '
             'невозможно точно определить корень x${index + 1}\n');
+
         /// return false, because accurate answer never be given
         return false;
       }
+
       /// switch rows if this need
       if (k != index) {
         var temp = matrix.grid[k];
         matrix.grid[k] = matrix.grid[index];
         matrix.grid[index] = temp;
+
         /// print matrix with message, that implements
         /// pseudo UI
         matrix.grid.printGrid(
@@ -122,16 +140,24 @@ class MatrixController {
 
       /// do normal equality
       for (var i = k; i < matrix.size; i++) {
+        /// get divide coefficient in iterated row
         var temp = matrix.grid[i][k];
 
-        /// for zero coefficient
+        /// if divide by zero continue loop
         if (temp.abs() < epsilon) {
           continue;
         }
+
+        /// if [temp] coefficient is {1},
+        /// then skip the division of the rows
         if (temp != 1) {
           for (var j = k; j < matrix.size + 1; j++) {
+            /// divide iterated row on the [temp] coefficient
             matrix.grid[i][j] /= temp;
           }
+
+          /// print matrix with message, that implements
+          /// pseudo UI
           matrix.grid.printGrid(
             message: 'поделим ${i + 1}-ую строку '
                 'на ${temp.toStringAsFixed(fraction)}',
@@ -139,13 +165,17 @@ class MatrixController {
           );
         }
 
-        /// equality don't sub self
+        /// continue loop if row tries sub self
         if (i == k) {
           continue;
         }
         for (var j = k; j < matrix.size + 1; j++) {
+          /// sub rows to zeroing rows
           matrix.grid[i][j] -= matrix.grid[k][j];
         }
+
+        /// print matrix with message, that implements
+        /// pseudo UI
         matrix.grid.printGrid(
           message: 'вычтем ${k + 1}-ую строку '
               'из ${i + 1}-${i == 2 ? 'ей' : 'ой'}',
@@ -153,40 +183,65 @@ class MatrixController {
         );
       }
     }
+
+    /// return true, if matrix has been successfully converted
     return true;
   }
 
+  /// this function find answers and push it to answer column
+  /// then print answers
   void printAnswers() {
-    /// fill answer row
+    /// put the last element of matrix to answer column
     xx[matrix.size - 1] = matrix.grid[matrix.size - 1][matrix.size];
     for (var i = matrix.size - 2; i >= 0; i--) {
+      /// declare answer column [i]
       xx[i] = matrix.grid[i][matrix.size];
       for (var j = i + 1; j < matrix.size; j++) {
+        /// get truly value of answer column [i]
         xx[i] -= matrix.grid[i][j] * xx[j];
       }
     }
 
-    /// print answers
+    /// print message to console, this
+    /// implements pseudo UI
     print('\nКорни системы:');
+
+    /// declare index of answer
     var i = 0;
+
+    /// fetch the answer column
     for (var x in xx) {
+      /// increment answer's index
       i++;
+
+      /// print answer as special message
+      /// to console, this implements pseudo UI
       print('x$i = ${x.toStringAsFixed(fraction)}');
     }
   }
 
+  /// declare of unnamed constructor
   MatrixController({
     required this.matrix,
     this.epsilon = 0.00001,
-  })
-      : xx = List.filled(
-    matrix.size,
-    0,
-  ),
+  })  : xx = List.filled(
+          /// fill answer column with zero
+          /// fields as default values
+          matrix.size,
+          0,
+        ),
+        /// get [fraction] by [epsilon] value once
+        /// at [this] object has created
         fraction = epsilon
             .toString()
+            /// get only part after the dot
+            /// in [epsilon] string
             .substring(
-          epsilon.toString().indexOf('.'),
-        )
+              /// get the dot position
+              /// in [epsilon] string
+              epsilon.toString().indexOf('.'),
+            )
+            /// get the length of the subbed
+            /// [epsilon] string
             .length;
 }
